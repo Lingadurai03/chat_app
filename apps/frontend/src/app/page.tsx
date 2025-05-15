2a0a5bac08b756f59c04c01abc360bde60ae1d39
Lingadurai03 <lingadurai033@gmail.com>
2025-05-15 23:14:00 +0530

    Add: Login Page

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,3 +1,9 @@
+import { LoginForm } from '@/components';
+
 export default function Home() {
-    return <div>Helllo world</div>;
+    return (
+        <div className='min-h-screen flex items-center justify-center bg-gray-100 p-4'>
+            <LoginForm />
+        </div>
+    );
 }
